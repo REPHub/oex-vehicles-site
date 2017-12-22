3ba059038f38ac3ecb433d9d277b1d1dcde42c41
shparri2 <shparri2@ncsu.edu>
2017-12-21 21:10:48 -0500

    added serviceworker, fixed long display for needs, and added location switcher for different location in the future :check:

--- js/index.js
+++ js/index.js
@@ -1,15 +1,34 @@
+// Initialize Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('./sw.js').then(function (registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope)
+    }, function (err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err)
+    })
+  })
+}
+
 const server = 'https://oex.glitch.me'
 const V = new Vue({
   data() {
     return {
+      location: 'NC',
+      locations: [
+        { text: 'NC' },
+        { text: 'CT' }
+      ],
+      title: 'Vehicle Status',
       data: { cars: null, needs: null },
-      activeCars: true,
+      activeCars: false,
       drawer: false,
       mini: true,
       dialog: false,
       navItems: [
         {
-          title: 'NC Cars',
+          title: 'Vehicles',
           icon: 'directions_car',
           function: () => {
             V.loadCars()
@@ -18,7 +37,7 @@ const V = new Vue({
           }
         },
         {
-          title: 'NC Needs',
+          title: 'Needs',
           icon: 'shopping_cart',
           function: () => {
             V.loadNeeds()
@@ -33,14 +52,31 @@ const V = new Vue({
             V.dialog = true
             V.drawer = false
           }
+        },
+        {
+          title: 'Shop Supplies',
+          icon: 'build',
+          href: 'https://trello.com/c/VdYyLUDh/5-materials-needed'
         }
-
       ]
+    }
+  },
+  computed: {
+    locationTitle: function () {
+      return this.title + ' ' + this.location
+    }
+  },
+  watch: {
+    // whenever question changes, this function will run
+    location: function () {
+      if (this.location === 'CT') {
+        alert(this.location + ' (Conneticut) Information is not prepared yet, so we will show NC data for now')
+      }
     }
   },
   methods: {
     loadCars(x) {
-      fetch(server + '/nc/cars')
+      fetch(server + `/${this.location.toLowerCase()}/cars`)
         .then(resp => resp.json())
         .then(data => {
           this.data.cars = data
@@ -48,12 +84,10 @@ const V = new Vue({
         })
     },
     loadNeeds() {
-      fetch(server + '/nc/needs')
+      fetch(server + `/${this.location.toLowerCase()}/needs`)
         .then(resp => resp.json())
         .then(data => {
-          // Add Checkbox model
-          data.forEach(x => x.checked = false)
-          // Get the Lists and put them under their cars
+          data.forEach(x => x.checked = false) // Checkbox's modle
           var nestedList = []
           this.data.cars.forEach(car => {
             var carNeeds = { name: car.name, id: car.id, needs: [] }
@@ -62,17 +96,15 @@ const V = new Vue({
           })
           // This Is the supplies list list
           nestedList.push({ name: 'Shop Supplies', id: '', needs: data.filter(need => need.cardId === '5a15e520c678987acecd583f') })
-          // Set it to the data
-          this.data.needs = nestedList
+          this.data.needs = nestedList // Inset into data store
         })
     },
     checkItem(id, cardId) {
-      var loc = 'nc'
-      // Currently Gets the correct card, or the last card which is the supplies list
-      var cardReference = this.data.needs.find(x => x.id === cardId) || this.data.needs[this.data.needs.length - 1]
+      // Gets the associated card, or the last card (which is the supplies list)
+      var cardReference = this.data.needs.find(x => x.id === cardId) || this.data.slice(-1)[0]
       var needChecked = cardReference.needs.find(x => x.id === id).checked
       var state = needChecked ? 'complete' : 'incomplete'
-      var url = server + `/nc/check?loc=${loc}&state=${state}&cardId=${cardId}&checkitemId=${id}`
+      var url = server + `/${this.location.toLowerCase()}/check?loc=${this.location.toLowerCase()}&state=${state}&cardId=${cardId}&checkitemId=${id}`
       console.log(url)
       fetch(url).then(resp => console.log(resp.json()))
     }
